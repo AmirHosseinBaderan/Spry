4d94570a842bd653d4e7ccf843a72549eac31be0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-07-20 17:00:47 +0330

    update : use dotnet 8

--- Spry.Console/Program.cs
+++ Spry.Console/Program.cs
@@ -1,57 +1,57 @@
-﻿namespace Spry.Console
+﻿using System;
+
+namespace Spry.ConsoleApp;
+
+public class Program
 {
-    using System;
-
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var myDto = new MyDto { Id = 1, IsDeleted = true };
+        var myDto = new MyDto { Id = 1, IsDeleted = true };
 
-            Spry.Select<MyDto>().Column(_ => myDto.Id).Column(_ => myDto.IsDeleted).From("tt")
-                .InnerJoin("table2", "audit").On("c1", "d1")
-                .InnerJoin("table3", "audit").On("c2", "d2")
-                .InnerJoin("table4", "audit").On("c4", "d2")
-                .InSchema("review")
-                .Where(_ => myDto.Id).EqualTo(1)
-                .AndWhere(_ => myDto.Id).InBetween(1, 10)
-                .AndWhere(_ => myDto.Id).GreaterThan(5)
-                .Build();
+        var query = Spry.Select<MyDto>().Column(_ => myDto.Id).Column(_ => myDto.IsDeleted).From("tt")
+              .InnerJoin("table2", "audit").On("c1", "d1")
+              .InnerJoin("table3", "audit").On("c2", "d2")
+              .InnerJoin("table4", "audit").On("c4", "d2")
+              .InSchema("dbo")
+              .Where(_ => myDto.Id).EqualTo(1)
+              .AndWhere(_ => myDto.Id).InBetween(1, 10)
+              .AndWhere(_ => myDto.Id).GreaterThan(5)
+              .Build();
 
-            Spry.InsertInto("tableOne", "review")
-                .Value("One", 1)
-                .Value(_ => myDto.Id)
-                .OutputIdentity()
-                .Execute(null);
+        Console.WriteLine(query);
+        //Spry.InsertInto("tableOne", "review")
+        //    .Value("One", 1)
+        //    .Value(_ => myDto.Id)
+        //    .OutputIdentity()
+        //    .Execute(null);
 
-            Spry.Update("tableOne")
-                .Set(_ => myDto.Id)
-                .Where<int>("id").EqualTo(1)
-                .Execute(null);
+        //Spry.Update("tableOne")
+        //    .Set(_ => myDto.Id)
+        //    .Where<int>("id").EqualTo(1)
+        //    .Execute(null);
 
-            Console.ReadLine();
-        }
+        Console.ReadLine();
     }
+}
 
-    public class MyDto
-    {
-        public int Id { get; set; }
+public class MyDto
+{
+    public int Id { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        public DateTime Today { get; set; }
+    public DateTime Today { get; set; }
 
-        public bool IsDeleted { get; set; }
-    }
+    public bool IsDeleted { get; set; }
+}
 
-    public enum EvidenceStatus : uint
-    {
-        NotApplicable = 0,
+public enum EvidenceStatus : uint
+{
+    NotApplicable = 0,
 
-        Requested = 1,
+    Requested = 1,
 
-        Awaiting = 2,
+    Awaiting = 2,
 
-        Received = 3
-    }
+    Received = 3
 }
